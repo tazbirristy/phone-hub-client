49b2bee60b05576fd8325c8f702e43602e0c819c
tazbirristy <tazbirristyweb@gmail.com>
2022-11-29 08:23:10 +0600

    seller add items

--- src/routes/SellersRoute/SellersRoute.js
+++ src/routes/SellersRoute/SellersRoute.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
+import Loader from "../../context/Loader/Loader";
+import useSeller from "../../hooks/useSeller";
 
-const SellersRoute = () => {
+const SellersRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const [isSeller, isSellerLoading] = useSeller(user?.email);
+  const location = useLocation();
+  if (loading || isSellerLoading) {
+    return <Loader></Loader>;
+  }
+  if (user && isSeller) {
+    return children;
+  }
   return (
-    <div>
-      <h1>SellersRoute</h1>
-    </div>
+    <Navigate to="/login" location={{ from: location }} replace></Navigate>
   );
 };
 
